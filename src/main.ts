5b803dd10a6598bca64454a778e2b3acc579ce11
abxlu <arjunbalu@hotmail.com>
2023-11-24 18:22:23 +0000

    added buttons and minor functionality to html and TypeScript

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,88 @@
 import './main.scss'
+
+const displayNumber= (outputNumber: number ): number => {
+
+}
+
+const buttons = document.querySelectorAll(".buttons__operations");
+
+
+
+const buttonFour = document.getElementById("button-4")
+
+
+buttonFour.addEventListener("click", ()=> {
+console.log("4")
+})
+
+const buttonFive = document.getElementById("button-5")
+console.log(buttonFive)
+
+buttonFive.addEventListener("click", ()=> {
+console.log("5")
+})
+
+const buttonSix = document.getElementById("button-6")
+console.log(buttonFive)
+
+buttonSix.addEventListener("click", ()=> {
+console.log("6")
+})
+
+const buttonMultiply = document.getElementById("button-x")
+console.log(buttonMultiply)
+
+buttonMultiply.addEventListener("click", ()=> {
+console.log("x")
+})
+
+const buttonDivide = document.getElementById("button-÷")
+console.log(buttonDivide)
+
+buttonDivide.addEventListener("click", ()=> {
+console.log("÷")
+})
+
+const buttonOne = document.getElementById("button-1")
+console.log(buttonOne)
+
+buttonOne.addEventListener("click", ()=> {
+console.log("1")
+})
+
+const buttonTwo = document.getElementById("button-2")
+console.log(buttonTwo)
+
+buttonTwo.addEventListener("click", ()=> {
+console.log("2")
+})
+
+const buttonThree = document.getElementById("button-3")
+console.log(buttonThree)
+
+buttonThree.addEventListener("click", ()=> {
+console.log("3")
+})
+
+const buttonAdd = document.getElementById("button-+")
+console.log(buttonAdd)
+
+buttonAdd.addEventListener("click", ()=> {
+console.log("+")
+})
+
+const buttonMinus = document.getElementById("button--")
+console.log(buttonMinus)
+
+buttonMinus.addEventListener("click", ()=> {
+console.log("-")
+})
+
+const buttonZero = document.getElementById("button-0")
+console.log(buttonZero)
+
+buttonZero.addEventListener("click", ()=> {
+console.log("0")
+})
+
+
