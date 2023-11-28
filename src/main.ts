537484aee9c297fb625ca7289499905c10f56b7d
abxlu <arjunbalu@hotmail.com>
2023-11-28 22:36:03 +0000

    displaying buttons, decimals and screenClear

--- src/main.ts
+++ src/main.ts
@@ -1,114 +1,79 @@
-// UMXclusive: Object Values (Initialisation)
-
+// Object Values (Initialisation)
 const calculator = {
-    displayValue: "0",
-    firstOperand: false,
+    displayValue: '0',
+    firstOperand: null,
     secondOperand: false,
     operator: null
 };
 
 // Display - Here is a function to update the display screen
-
 const updateScreen = () => {
 const display = document.querySelector(".screen");
     display.value = calculator.displayValue;
 };
+updateScreen();
 
-updateScreen(3);
-
-// Key press
-
-const buttonFour = document.getElementById("button-4")
-
-buttonFour.addEventListener("click", ()=> {
-calculator.displayValue = "4";
-})
-
+// Handling Key presses
 const keys = document.querySelector(".keys")
-keys.addEventListener("click", ()=> {
-const {target} = event;
-if(!target.matches("button")) {
-    return;
-}
+keys.addEventListener("click", (event)=> {
+    const {target} = event;
+    if(!target.matches("button")) {
+        return;
+    }
+
+    if(target.classList.contains("operator")) {
+        handleOperator(target.value);
+        updateScreen();
+        return;
+    }
+
+    if(target.classList.contains("decimal")) {
+        inputDecimal(target.value);
+        updateScreen();
+        return;
+    }
+
+    if(target.classList.contains("all-clear")) {
+        resetCalculator();
+        updateScreen();
+        return;
+    }
+
+    inputDigit(target.value);
+    updateScreen();
 });
 
+// Input Digit Value
+const inputDigit = (digit) => {
+    const { displayValue, secondOperandPending } = calculator;
 
+    if(calculator.secondOperandPending === true) {
+        calculator.displayValue = digit;
+        calculator.secondOperandPending = false
+    } 
+    
+    else {
+        calculator.displayValue = 
+        displayValue === "0" ? digit : displayValue + digit;
+    }
+};
 
+// Input Decimal Value
+const inputDecimal = (dot) => {
+    if (calculator.secondOperandPending === true) {
+        calculator.displayValue = "0.";
+        calculator.secondOperandPending = false;
+        return;
+    }
+    if (!calculator.displayValue.includes(dot)) {
+        calculator.displayValue += dot;
+    }
+};
 
-
-
-
-const buttonFive = document.getElementById("button-5")
-
-
-buttonFive.addEventListener("click", ()=> {
-    inputScreen.value = "5";
-})
-
-const buttonSix = document.getElementById("button-6")
-
-
-buttonSix.addEventListener("click", ()=> {
-    inputScreen.value = "6";
-})
-
-const buttonMultiply = document.getElementById("button-x")
-console.log(buttonMultiply)
-
-buttonMultiply.addEventListener("click", ()=> {
-console.log("x")
-})
-
-const buttonDivide = document.getElementById("button-÷")
-console.log(buttonDivide)
-
-buttonDivide.addEventListener("click", ()=> {
-console.log("÷")
-})
-
-const buttonOne = document.getElementById("button-1")
-console.log(buttonOne)
-
-buttonOne.addEventListener("click", ()=> {
-console.log("1")
-})
-
-const buttonTwo = document.getElementById("button-2")
-console.log(buttonTwo)
-
-buttonTwo.addEventListener("click", ()=> {
-console.log("2")
-})
-
-const buttonThree = document.getElementById("button-3")
-console.log(buttonThree)
-
-buttonThree.addEventListener("click", ()=> {
-console.log("3")
-})
-
-const buttonAdd = document.getElementById("button-+")
-console.log(buttonAdd)
-
-buttonAdd.addEventListener("click", ()=> {
-console.log("+")
-})
-
-const buttonMinus = document.getElementById("button--")
-console.log(buttonMinus)
-
-buttonMinus.addEventListener("click", ()=> {
-console.log("-")
-})
-
-const buttonZero = document.getElementById("button-0")
-console.log(buttonZero)
-
-buttonZero.addEventListener("click", ()=> {
-console.log("0")
-})
-
-
-
-
-
+// Reset Calculator
+const resetCalculator = () => {
+    calculator.displayValue = "0";
+    calculator.firstOperand = null;
+    calculator.secondOperandPending = false;
+    calculator.operator = null;
+};
